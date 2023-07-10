808c5326ed6f3447913855bac99305788778081a
fake-soul <bharatraj.mulchandani@gmail.com>
2023-07-10 17:02:06 +0530

    Added Web service

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func main1() {
+	fmt.Print("Hello, World!")
+}
 
 func main() {
-	fmt.Print("Hello, World!")
+	//	 set a web server
+	//	 listen to port 8080
+	//	 handle request
+	//	 return response
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World this is web Service in GO!")
+	})
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "home.html")
+	})
+
+	http.ListenAndServe(":3000", nil)
 }
